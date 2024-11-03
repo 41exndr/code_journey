71b4a8582d37d23468b436cbacb9816be6749a77
41exndr <RVAlexander0@gmail.com>
2024-11-03 18:41:27 -0400

    Update

---
+++ Test/Test5/test5.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path, PureWindowsPath
+from os import system
+
+mi_ruta = Path("/home/jin/Documents/Projects/python_learning/Test/Test5/Recetas")
+
+def inicio():
+    return
+
+
+menu = 0
+
+#Mostrar menú de inicio 1 a 6 opciones
+
+if menu == 1: #leer receta
+    #motras categorías
+    #elegir categoría
+    #mostrar recetas
+    #elegir recetas
+    #leer receta
+    #volver inicio
+    pass
+elif menu == 2: #Crear receta nueva
+    #motras categorías
+    #elegir categoría
+    #crear receta nueva
+    #volver inicio
+    pass
+elif menu == 3: #crear categoría nueva
+    #crear categoría nueva
+    #volver a inicio
+    pass
+elif menu == 4:
+    #motras categorías
+    #elegir categoría
+    #mostrar recetas
+    #elegir recetas
+    #eliminar receta
+    #volver inicio
+    pass
+elif menu == 5: #eliminar categoría
+    #motras categorías
+    #elegir categoría
+    #eliminar categoría
+    #volver inicio
+    pass
+elif menu == 6:
+    #finalizar programa
+    pass
