8698e34b92438c3462071ae9cec59921d1cb0cb8
41exndr <RVAlexander0@gmail.com>
2025-10-20 19:48:19 -0400

    learning java

---
+++ projects/Diplomado/Java/Creator.java
@@ -0,0 +1,28 @@
+public class Creator {
+	public static void main(String[] args) {
+    String name = "James Gosling";
+    int yearCreated = 1995;
+    byte age = 45;
+    char letter = 'A';
+    boolean test = false;
+    double decimal = 3.14159265359;
+    String openingLyrics = ("\"Yesterday, all my troubles seemed so far away\""); 
+    System.out.println("This is the backslash symbol: \\");
+    System.out.println("Hello\nGoodbye");
+    System.out.println(openingLyrics);
+	}
+}
+
+/*
+byte smallNumber = 100;
+short mediumNumber = 30000;
+int largeNumber = 2000000000;
+long veryLargeNumber = 9223372036854775807L;
+float decimalNumber = 3.14f;
+double preciseDecimal = 3.14159265359;
+boolean isJavaFun = true;
+char letter = 'A'; single quotes
+
+The \" escape sequence allows us to add quotation marks " to a String value.
+
+*/
