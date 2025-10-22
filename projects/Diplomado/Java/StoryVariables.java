ecadb3ce597f3a3ccf68175b5dfa16110be97d9a
41exndr <RVAlexander0@gmail.com>
2025-10-22 07:27:20 -0400

    Learning more java

---
+++ projects/Diplomado/Java/StoryVariables.java
@@ -0,0 +1,32 @@
+public class StoryVariables {
+  /*
+    Author: 41exndr
+    Date: /19/2049
+    This program generates a mad libbed story.
+  */
+  	public static void main(String[] args){
+      
+        String name1 = "Alex";
+        String adjective1 = "big";
+        String adjective2 = "short";
+        String adjective3 = "pretty";
+        String verb1 = "run";
+        String noun1 = "bread";
+        String noun2 = "table";
+        String noun3 = "microwave";
+        String noun4 = "microphone";
+        String noun5 = "computer";
+        String noun6 = "phone";
+        String name2 = "Victoria";
+        int number = 6;
+        String place1 = "Chacao";
+
+
+        
+        {
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+    
+        System.out.println(story);
+        }
+    }       
+}
--- projects/Diplomado/Java/text.java
+++ projects/Diplomado/Java/text.java
@@ -9,4 +9,13 @@ ass
 comment
 example
 its official name is "JAVADOC COMMENT"
+
+We can use compound assignment operators for all of the arithmetic operators we’ve covered:
+
+    Addition (+=)
+    Subtraction (-=)
+    Multiplication (*=)
+    Division (/=)
+    Modulo (%=)
+
  */
