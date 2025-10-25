4151e78b3fc02f0393e8f8d02db09666a0486a10
41exndr <RVAlexander0@gmail.com>
2025-10-25 14:29:18 -0400

    java learning OOP Objects, classes, constructors, methods, parameters.

---
+++ projects/Diplomado/Java/Store.java
@@ -0,0 +1,15 @@
+public class Store {
+    /* constructor, usualmente se llama igual que la clase*/
+    public Store(){
+        System.out.println("I am inside the Store constructor");
+    }
+}
+
+
+/*
+public class Main{
+  public static void main(String[] args){
+    Store newStore = new Store();
+  }
+}
+ */
---
+++ projects/Diplomado/Java/dog.java
@@ -0,0 +1,24 @@
+public class dog {
+  String breed;
+  boolean hasOwner;
+  int age;
+  
+  public dog(String dogBreed, boolean dogOwned, int dogYears) {
+    System.out.println("Constructor invoked!");
+    breed = dogBreed;
+    hasOwner = dogOwned;
+    age = dogYears;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Main method started");
+    dog fido = new dog("poodle", false, 4);
+    dog nunzio = new dog("shiba inu", true, 12);
+    boolean isFidoOlder = fido.age > nunzio.age;
+    int totalDogYears = nunzio.age + fido.age;
+    System.out.println("Two dogs created: a " + fido.breed + " and a " + nunzio.breed);
+    System.out.println("The statement that fido is an older dog is: " + isFidoOlder);
+    System.out.println("The total age of the dogs is: " + totalDogYears);
+    System.out.println("Main method finished");
+  }
+}
